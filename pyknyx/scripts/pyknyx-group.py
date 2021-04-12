2fee654e797b3110d61c49edbe01449616a73d35
Simón Golpe <33048138+Golpe82@users.noreply.github.com>
2021-04-12 15:28:53 +0200

    Add parentheses to the print statement

--- pyknyx/scripts/pyknyx-group.py
+++ pyknyx/scripts/pyknyx-group.py
@@ -395,8 +395,8 @@ def main():
     # If XML map file name is given, try to load it
     if args.xmlMapFile:
         mapper.loadXML(args.xmlMapFile)
-        for gad,tab in mapper.table.items():
-            print gad,tab
+        for gad, tab in mapper.table.items():
+            print(gad, tab)
     else:
         mapper.loadFrom(args.gadMapPath)
 
